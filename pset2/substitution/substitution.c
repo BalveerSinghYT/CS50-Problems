861a5b0e55d7f6208429ab08eae1982cef15364a
Balveer Singh <balveersinghyt@gmail.com>
2020-10-02 23:40:04 +0530

    Final Submission of Substitution problem.
    pset2/substitution:- Completed
    Testing result:- +ve

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -5,8 +5,9 @@
 #include<ctype.h>
 
 int validate_key(string argv);
-string encrypt_message(string argv, string ptext);
+void encrypt_message(string argv, string ptext);
 
+//---------------------- MAIN ----------------------
 int main(int argc, string argv[])
 {
     if (argc != 2)
@@ -20,21 +21,19 @@ int main(int argc, string argv[])
         return 1;
     }
     
-    if(validate_key(argv[1]) == 0)
-    {
-        printf("True\n");
-    }
+    if(validate_key(argv[1]) == 1)
+        return 1;
     
     string plaintext = get_string("plaintext: ");
     string ciphertext;
-    printf("%s\n", encrypt_message(argv[1], plaintext));
-    
-    
+    printf("ciphertext: ");
+    encrypt_message(argv[1], plaintext);
     
     return 0;
 }
 
-string encrypt_message(string argv, string ptext)
+//---------------------- ENCRYPT MESSAGE ----------------------
+void encrypt_message(string argv, string ptext)
 {
     int note;
     int ptext_size = strlen(ptext);
@@ -44,59 +43,73 @@ string encrypt_message(string argv, string ptext)
     string Alpha_Array = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     for (int i=0; i<ptext_size; i++)
     {
-        // ------------------------------- UNDER CONSTRUCTION ------------------------------------------------
         for (int j=0; j<26; j++)
         {
-            if (ptext[i] == alpha_array[j]) // check at what place character is present in real alpha_array
+            if (ptext[i] == (char) 97 + j || ptext[i] == (char) 65+j) // check at what place character is present in real alpha_array
             {
-                note = j;
-                //printf("note = %i\n", j);
-                printf("%c", argv[note]);
+                if(isupper(ptext[i]))
+                    printf("%c", toupper(argv[j]));
+                    
+                else if(islower(ptext[i]))
+                    printf("%c", tolower(argv[j]));
             }
-            else if (ptext[i] == Alpha_Array[j])
+            
+            else if (ptext[i] == (char)48+j )
             {
-                note = j;
-                printf("%c", argv[note]);
+                if(j>10)
+                {
+                    break;
+                }
+                printf("%c", ptext[i]);
             }
-           
         }
-        //____________________________ ///////////////////////////-----------------------------------------------
+        
+        if (ptext[i] == ',' || ptext[i] == ' ')
+            printf("%c", ptext[i]);
+
+        else if (ptext[i] == '!' || ptext[i] == '.')
+            printf("%c", ptext[i]);
+            
     }
-    return encrypt;
+    printf("\n");
+    
 }
 
+//---------------------- VALIDATE KEY ----------------------
 int validate_key(string argv)
 {
     int count = 0;
     int repeat = 0;
-    
-    //--------------------- Covert all chracter to upper ----------------------------------
+
+    //**** Covert all chracter to upper ****
     int k=0;
     char ch;
-    char str[26]; 
-    strcpy(str,argv); 
+    char str[26];
+    strcpy(str,argv);
     //printf("str= %s\n", str);
-    char s[26]; 
+    char s[26];
     while (k<27)
     {
         ch = str[k];
         s[k] = (char)toupper(ch);
         k++;
     }
-    //-------------
-    printf("%s\n", s);
-    for(int i=0; i<26; i++)
+    //---- **** ----
+    
+    //printf("%s\n", s);
+    for (int i=0; i<26; i++)
     {
-        
-        for ( int j=0; j<26; j++) // compare each chracter to all capital & small alphabets
+        for (int j=0; j<26; j++) // compare each chracter to all capital & small alphabets
         {
             if ((int)argv[i] == 65+j || (int)argv[i] == 97+j)
             {
                 count++;        // count help us to idetifiy all character are alphabets
             }
+            
         }
-        
-        for (int j=0; j<26; j++)  // check how many times a chracter is repeating
+        //printf("%i\n", count);
+
+        for (int j=0; j<26; j++) // check how many times a chracter is repeating
         {
             if((int)s[i] == s[j])
             {
@@ -110,14 +123,13 @@ int validate_key(string argv)
         }
         repeat = 0;
     }
- 
+
     if(count == 26)
         return 0;
-    
+
     else
     {
         printf("Key should not contain any numeric chracter\n");
         return 1;
     }
 }
-
